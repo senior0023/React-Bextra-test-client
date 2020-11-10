37de75c33d2e6c3c817867e3208629b6c80fdb8a
senior0023 <ared2110@gmail.com>
2020-11-10 22:32:28 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
+import Axios from 'axios';
 
 function App() {
+
+  // data state to store the Accounts data. Its initial value is an empty array
+  const [ data, setData ] = useState([]);
+
+  // Using useEffect to call the API once mounted and set the data
+  useEffect(() => {
+    (async () => {
+      const result = await Axios('http://localhost:8080/api/accounts');
+      console.log('result: ', result.data);
+      setData(result.data);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
