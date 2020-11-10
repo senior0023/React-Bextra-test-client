be5deeae8f65949b6d5739de82f937ade3d624f3
senior0023 <ared2110@gmail.com>
2020-11-11 00:42:03 +0800

    implement basic functionality of filtering, sorting, grouping using react-table hooks

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,6 @@
 // Table.js
-import React from 'react';
-import { useTable } from 'react-table';
+import React, { useState, useEffect } from 'react';
+import { useTable, useFilters, useSortBy, useGropuBy, useGroupBy, useExpanded } from 'react-table';
 
 export default function Table({ columns, data }) {
     // Use the useTable Hook to send the columns and data to build the table
@@ -10,38 +10,107 @@ export default function Table({ columns, data }) {
         headerGroups, // headerGropus, if the table has groupings
         rows, // rows for the table based on the data passed
         prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
-    } = useTable({
-        columns, 
-        data
-    });
+        setFilter, 
+        state: { groupBy, expanded }
+    } = useTable(
+        {
+            columns, 
+            data
+        },
+        useFilters,
+        useGroupBy,
+        useSortBy,
+        useExpanded
+    );
+
+    const [ filterInput, setFilterInput ] = useState("");
+
+    // update the filter state when input changes
+    const handleFilterChange = e => {
+        const value = e.target.value || "";
+        setFilter("name", value);
+        setFilterInput(value);
+    };
 
     return (
-        <table {...getTableProps()} className="styled-table">
-            <thead>
-                {
-                    headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th {...column.getHeaderProps()}>{ column.render("Header") }</th>
-                            ))}
-                        </tr>
-                    ))
-                }
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {
-                    rows.map((row, i) => {
-                        prepareRow(row); // This line is necessary to prepare the rows and get the row props from react-table dynamically
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return <td {...cell.getCellProps()}>{ cell.render("Cell") }</td>;
-                                })}
+        <div className="table-container">
+            <input
+                value={ filterInput }
+                onChange={handleFilterChange}
+                placeholder={"Search by name..."}
+                className="table-filter"
+            />
+            <table {...getTableProps()} className="styled-table">
+                <thead>
+                    {
+                        headerGroups.map(headerGroup => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map(column => (
+                                    <th 
+                                        {...column.getHeaderProps(column.getSortByToggleProps())}
+                                        className={
+                                            column.isSorted ? (column.isSortedDesc ? "sort-desc" : "sort-asc") : ""
+                                        }
+                                    >
+                                        { column.render("Header") }
+                                        {
+                                            column.canGroupBy ? (
+                                                <span {...column.getGroupByToggleProps()}>
+                                                    {column.isGrouped ? '🛑 ' : '👊 '}
+                                                </span>
+                                            ) : null
+                                        }
+                                    </th>
+                                ))}
                             </tr>
-                        );
-                    })
-                }
-            </tbody>
-        </table>
+                        ))
+                    }
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {
+                        rows.map((row, i) => {
+                            prepareRow(row); // This line is necessary to prepare the rows and get the row props from react-table dynamically
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map(cell => {
+                                        return (
+                                            <td 
+                                                {...cell.getCellProps()}
+                                                style={{
+                                                    background: cell.isGrouped
+                                                        ? '#0aff0082'
+                                                        : cell.isAggregated
+                                                        ? '#ffa50078'
+                                                        : cell.isPlaceholder
+                                                        ? '#ff000042'
+                                                        : 'white',
+                                                }}
+                                            >
+                                                {cell.isGrouped ? (
+                                                    // If it's a grouped cell, add an expander and row count
+                                                    <>
+                                                    <span {...row.getToggleRowExpandedProps()}>
+                                                        {row.isExpanded ? 'v' : '>'}
+                                                    </span>{' '}
+                                                    {cell.render('Cell')} ({row.subRows.length})
+                                                    </>
+                                                ) : cell.isAggregated ? (
+                                                    // If the cell is aggregated, use the Aggregated
+                                                    // renderer for cell
+                                                    cell.render('Aggregated')
+                                                ) : cell.isPlaceholder ? null : ( // For cells with repeated values, render null
+                                                    // Otherwise, just render the regular cell
+                                                    cell.render('Cell')
+                                                )}
+                                            </td>
+                                        );
+                                    })}
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     );
 }
